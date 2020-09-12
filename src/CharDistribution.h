2d2920142eeb1cdd46bfc637dbd26dd1cea981d0
nkari <nkari82@gmail.com>
2020-09-12 13:27:39 +0900

    Fixed wrong character ranges check for SJIS. https://github.com/aadsm/jschardet/commit/b195a30506bf3341311b4816605a44b8a1567411

--- src/CharDistribution.h
+++ src/CharDistribution.h
@@ -202,8 +202,8 @@ public:
   SJISDistributionAnalysis();
 protected:
   //for sjis encoding, we are interested 
-  //  first  byte range: 0x81 -- 0x9f , 0xe0 -- 0xfe
-  //  second byte range: 0x40 -- 0x7e,  0x81 -- oxfe
+  //  first  byte range: 0x81 -- 0x9f , 0xe0 -- 0xef
+  //  second byte range: 0x40 -- 0x7e,  0x80 -- 0xfc
   //no validation needed here. State machine has done that
   PRInt32 GetOrder(const char* str) 
   { 
@@ -215,7 +215,7 @@ protected:
     else
       return -1;
     order += (unsigned char)*(str+1) - 0x40;
-    if ((unsigned char)str[1] > (unsigned char)0x7f)
+	if ((unsigned char)str[1] < 0x40 || (unsigned char)str[1] == 0x7F || (unsigned char)str[1] > 0xFC)
       order--;
     return order;
   }
